45e713f9eb390a4304d5f3db74ca50f8c6868842
Rao Guttula <rao@Raos-MacBook-Pro.local>
2018-11-27 17:37:07 -0800

    refactored code includes front-end changes

--- model-psql/database.js
+++ model-psql/database.js
@@ -6,3 +6,65 @@
 // const query = client.query(
 //   'CREATE TABLE IF NOT EXISTS movieTimes(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
 // query.on('end', () => { client.end(); });
+
+
+const { Pool, Client } = require('pg');
+
+const pool = new Pool({
+  user: 'rao',
+  host: 'localhost',
+  database: 'movietimesdb',
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.log('error from connection.js line 11>>>', err);
+  } else {
+    console.log(null, res);
+  }
+  pool.end();
+});
+
+const client = new Client({
+  user: 'rao',
+  host: 'localhost',
+  database: 'movietimesdb',
+});
+client.connect();
+
+//***** DATABASE METHODS */
+
+// const getAll = (actor, callback) => {
+
+//   const query = {
+//     text: 'SELECT * FROM movies WHERE actor = $1',
+//     values: [actor],
+//   };
+
+//   client.query(query, (error, results) => {
+//     if (error) {
+//       callback(error);
+//     } else {
+//       callback(null, results);
+//     }
+//     // client.end();
+//   });
+// };
+
+// const addMovie = (title, year, image, actor, callback) => {
+//   const query = {
+//     text: 'INSERT INTO movies(title, year, image, actor) VALUES($1, $2, $3, $4)',
+//     values: [title, year, image, actor],
+//   };
+
+//   client.query(query, (error, results) => {
+//     if (error) {
+//       callback(error);
+//     } else {
+//       callback(null, results);
+//     }
+//     // client.end();
+//   });
+
+// };
+// module.exports = { getAll, addMovie };
